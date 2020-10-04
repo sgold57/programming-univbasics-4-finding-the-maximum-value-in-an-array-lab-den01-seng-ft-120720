5c12d77b8fe8fae4d700acf1b84ab28ea8634fc1
Sam Gold <sgold5757@gmail.com>
2020-10-04 18:05:17 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,9 @@
 def find_max_value(array)
-  # Add your solution here
+  number_to_beat = array[0]
+  for i in 1...array.size
+    if array[i] > number_to_beat
+      number_to_beat = array[i]
+    end
+  end
+  number_to_beat
 end
